f420dfba87173768fe5a6299cc2805be4401c00f
Philip Vuong <40782386+philipvuong@users.noreply.github.com>
2020-12-22 13:20:47 -0600

    added sessions - login/logout

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SESS_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
